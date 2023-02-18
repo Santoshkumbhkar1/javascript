76ee494a0c78eac0c22bbc3e80a21c136f0cdb6c
santosh kumbhkar <santosh05122001@gmail.com>
2023-02-18 09:09:17 +0530

    first commit of my git

---
+++ Functions/function.js
@@ -0,0 +1,18 @@
+
+function logger(){
+    console.log("hello from the javscript");
+}
+logger();
+
+
+function fruitprocessors( apple ,mango){
+
+    const juise = `The juise of ${apple} apples and ${mango} mango `;
+
+    return juise;
+}
+const result = fruitprocessors(5,3);
+console.log(result);
+
+const result2 = fruitprocessors(6 ,7);
+console.log(result2);
